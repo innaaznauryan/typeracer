573418e2d1282236356d5bb423c9afc5203efbef
innaaznauryan <inna.aznauryan@gmail.com>
2024-07-13 13:47:00 +0400

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <TypeRacer></TypeRacer>
+  <main>
+    <TypeRacer></TypeRacer>
+  </main>
+  <BaseFooter></BaseFooter>
 </template>
 
 <script setup lang="ts">
 import TypeRacer from "./components/TypeRacer.vue";
+import BaseFooter from "./components/BaseFooter.vue";
 </script>
---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,45 @@
+<template>
+    <footer>
+        <p>© {{ year }} Coded by Inna Aznauryan</p>
+        <p class="icons">
+            <a v-for="link in links" :href="link.href" target="_blank">
+                <component 
+                    :is="link.icon" 
+                    color="#646cff" 
+                    size="24" 
+                    stroke="2">
+                </component>
+            </a>
+        </p>
+    </footer>
+</template>
+
+<script setup lang="ts">
+import { IconBrandGithub } from '@tabler/icons-vue';
+import { IconBrandLinkedin } from '@tabler/icons-vue';
+
+const year = new Date().getFullYear();
+
+const links = [
+    {
+        href: "https://www.linkedin.com/in/inna-aznauryan/",
+        icon: IconBrandLinkedin
+    },
+    {
+        href: "https://github.com/innaaznauryan",
+        icon: IconBrandGithub
+    }
+]
+</script>
+
+<style scoped>
+footer {
+    padding-top: 40px;
+}
+.icons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+}
+</style>
